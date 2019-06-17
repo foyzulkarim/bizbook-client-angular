e7e0aa4ef9731b37bfae2368901282297111b843
animesh biswas <anibiswas@ael-bd.com>
2019-06-17 14:55:35 +0600

    shop entry / list

--- src/app/pages/setup/setup.module.ts
+++ src/app/pages/setup/setup.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { SetupComponent } from './setup.component';
 import { SetupRoutingModule } from './setup.routing.module';
 import { ShopComponent } from './shop/list/shop.component';
 import { ShopEntryComponent } from './shop/entry/shop-entry.component';
 
-const COMPONENTS = [SetupComponent, ShopComponent, ShopEntryComponent];
+import { PaginationComponent } from '../../component/pagination/pagination.component';
+
+const COMPONENTS = [
+  SetupComponent,
+  ShopComponent,
+  ShopEntryComponent,
+  PaginationComponent
+];
 
 const ENTRY_COMPONENTS = [];
 
 @NgModule({
-  imports: [FormsModule, SetupRoutingModule],
+  imports: [FormsModule, CommonModule, SetupRoutingModule],
   declarations: [...COMPONENTS],
   entryComponents: [...ENTRY_COMPONENTS]
 })
